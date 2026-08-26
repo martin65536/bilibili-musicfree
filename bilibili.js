62cd268bb9a303d3de2c8b37509476e9b16669ff
martin65536 <martin65536@outlook.com>
2026-08-26 18:43:34 +0800

    chore: srcUrl use github raw for instant refresh (v0.4.5)

--- bilibili.js
+++ bilibili.js
@@ -761,10 +761,10 @@ async function getLyric(musicItem) {
 module.exports = {
     platform: "bilibili",
     appVersion: ">=0.0",
-    version: "0.4.4",
+    version: "0.4.5",
     author: "猫头猫 (cookie+字幕扩展)",
     cacheControl: "no-cache",
-    srcUrl: "https://cdn.jsdelivr.net/gh/martin65536/bilibili-musicfree@v0.4.4/bilibili.js",
+    srcUrl: "https://raw.githubusercontent.com/martin65536/bilibili-musicfree/main/bilibili.js",
     primaryKey: ["id", "aid", "bvid", "cid"],
     userVariables: [
         {
